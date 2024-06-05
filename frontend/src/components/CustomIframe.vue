efdbf96e76c735667a40b859f8cadabcae3b2576
Zloooy <gu1999yra@yandex.ru>
2024-06-05 18:25:46 +0300

    Добавлен фронтенд и пайплайн для его загрузки на Yandex Cloud.

---
+++ frontend/src/components/CustomIframe.vue
@@ -0,0 +1,33 @@
+<template>
+    <iframe
+        class="custom-iframe"
+        :title="title"
+        :src="url"
+        allowfullscreen
+        frameborder="0"
+    />
+</template>
+
+
+<script setup lang="ts">
+import {toRefs} from 'vue';
+
+export interface Props {
+    url: string;
+    title: string;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+    title: 'Без названия'
+});
+const {title, url} = toRefs(props);
+
+</script>
+
+<style lang="scss" scoped>
+.custom-iframe {
+    position: relative;
+    height: 100%;
+    width: 100%;
+}
+</style>
---
+++ frontend/src/pages/table.vue
@@ -0,0 +1,11 @@
+<template>
+    <custom-iframe
+        name="Таблица"
+        :url="GOOGLE_SHEETS_URL"
+    />
+</template>
+
+<script setup lang="ts">
+    import {GOOGLE_SHEETS_URL} from '@/utils/env-variable-loader'
+    import '@/components/CustomIframe.vue'
+</script>
